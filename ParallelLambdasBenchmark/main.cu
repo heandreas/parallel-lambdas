3b910f94ed3516129b57a3465a735b59011e49b1
Andreas Henne <henne@fifty2.eu>
2019-09-24 21:28:09 +0200

    Worked on benchmarks to mimic sph-style neighbor sums.

--- ParallelLambdasBenchmark/main.cu
+++ ParallelLambdasBenchmark/main.cu
@@ -76,51 +76,106 @@ __global__ void opCudaNested(int n, T *x, float *y, F op)
 
 struct InputData
 {
-	float x;
+	uint64_t offset;
 	int numElements;
 };
 
 int main(void)
 {
-	int N = 1 << 25;
+	int N = 1 << 22;
 	std::cout << "Number of elements " << N << std::endl;
 
-	std::vector<InputData> input(N);
-	std::vector<float> y(N);
+	std::vector<InputData> cpuInput(N);
+	std::vector<int> cpuNeighborIndices;
+	std::vector<float> cpuFloats(N);
+	std::vector<float> cpuOutput(N);
 	std::vector<float> control(N);
+
+	InputData* gpuInput;
+	int* gpuNeighborIndices;
+	float* gpuFloats;
+	float* gpuOutput;
+	cudaMalloc(&gpuInput, N * sizeof(InputData));
+	cudaMalloc(&gpuFloats, N * sizeof(float));
+	cudaMalloc(&gpuOutput, N * sizeof(float));
 
 	// initialize x and y arrays on the host
 	for (int i = 0; i < N; i++)
 	{
-		input[i].numElements = 20 + i % 11;
-		input[i].x = (float)i;
+		int numElements = 20 + i % 11;
+		cpuInput[i].offset = cpuNeighborIndices.size();
+		cpuFloats[i] = (i % 52) * 0.1f;
+		int minI = std::max(0, i - numElements / 2);
+		int maxI = std::min(N, i + numElements / 2);
+		cpuInput[i].numElements = maxI - minI;
+		for (int index = minI; index < maxI; index++)
+		{
+			cpuNeighborIndices.push_back(index);
+		}
 	}
 
-	InputData* inputGPU;
-	float* yGPU;
-	cudaMalloc(&inputGPU, N * sizeof(InputData));
-	cudaMalloc(&yGPU, N * sizeof(float));
+	cudaMemcpy(gpuInput, cpuInput.data(), N * sizeof(InputData), cudaMemcpyHostToDevice);
+	cudaMemcpy(gpuFloats, cpuFloats.data(), N * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMalloc(&gpuNeighborIndices, cpuNeighborIndices.size() * sizeof(int));
+	cudaMemcpy(gpuNeighborIndices, cpuNeighborIndices.data(), cpuNeighborIndices.size() * sizeof(int), cudaMemcpyHostToDevice);
 
-	{
-		auto timer = Timer::logScopeTiming("Transfer CPU to GPU");
-		cudaMemcpy(inputGPU, input.data(), input.size() * sizeof(InputData), cudaMemcpyHostToDevice);
-	}
-
-	omp_set_num_threads(4);
+	omp_set_num_threads(omp_get_num_procs());
 	std::cout << "OMP num threads: " << omp_get_max_threads() << std::endl;
 
-	auto gpuLambda = []__host__ __device__(const InputData& data)
+	auto gpuLambda = [=] __device__(float* particles, const InputData& neighbors)
 	{
 		float accu = 0.0f;
-		for (int i = 0; i < data.numElements; i++)
-			accu += std::sqrt(data.x * (float)i);
+		int* neighborListEnd = gpuNeighborIndices + neighbors.offset + neighbors.numElements;
+		for (int* it = gpuNeighborIndices + neighbors.offset; it != neighborListEnd; it++)
+		{
+			float data = particles[*it];
+			if (data > 0.0f)
+				accu += 1.0f / std::sqrt(data);
+		}
 		return accu;
 	};
-	auto cpuLambda = [](const InputData& data)
+	auto cpuLambda = [&](float* particles, const InputData& neighbors)
 	{
 		float accu = 0.0f;
-		for (int i = 0; i < data.numElements; i++)
-			accu += std::sqrt(data.x * (float)i);
+		int* neighborListEnd = (int*)cpuNeighborIndices.data() + neighbors.offset + neighbors.numElements;
+		for (int* it = (int*)cpuNeighborIndices.data() + neighbors.offset; it != neighborListEnd; it++)
+		{
+			float data = particles[*it];
+			if (data > 0.0f)
+				accu += 1.0f / std::sqrt(data);
+		}
+		return accu;
+	};
+
+	auto cpuLambda2 = [&] (int i)
+	{
+		float accu = 0.0f;
+		int numNeighbors = 20 + i % 11;
+		int halfNeighbors = numNeighbors / 2;
+		int minI = max(0, i - halfNeighbors);
+		int maxI = min(N, i + halfNeighbors);
+		for (int index = minI; index < maxI; index++)
+		{
+			float data = cpuFloats[index];
+			if (data > 0.0f)
+				accu += 1.0f / std::sqrt(data);
+		}
+		return accu;
+	};
+
+	auto gpuLambda2 = [=] __device__(int i)
+	{
+		float accu = 0.0f;
+		int numNeighbors = 20 + i % 11;
+		int halfNeighbors = numNeighbors / 2;
+		int minI = max(0, i - halfNeighbors);
+		int maxI = min(N, i + halfNeighbors);
+		for (int index = minI; index < maxI; index++)
+		{
+			float data = gpuFloats[index];
+			if (data > 0.0f)
+				accu += 1.0f / std::sqrt(data);
+		}
 		return accu;
 	};
 
@@ -129,52 +184,49 @@ int main(void)
 		{
 			auto timer = Timer::logScopeTiming("Single threaded CPU");
 			for (int i = 0; i < N; i++)
-				y[i] = cpuLambda(input[i]);
+				cpuOutput[i] = cpuLambda(cpuFloats.data(), cpuInput[i]);
 		}
 		{
 			auto timer = Timer::logScopeTiming("Multi threaded CPU");
-			ParallelUtilsOpenMP::createTeam()->map(N, [&](size_t i) { y[i] = cpuLambda(input[i]); });
+			ParallelUtilsOpenMP::createTeam()->map(N, [&](size_t i) { cpuOutput[i] = cpuLambda(cpuFloats.data(), cpuInput[i]); });
 		}
 		{
-			auto timer = Timer::logScopeTiming("Multi threaded CPU with thrust");
-			thrust::transform(thrust::system::omp::par, input.begin(), input.end(), y.begin(), cpuLambda);
+			auto timer = Timer::logScopeTiming("Multi threaded CPU 2");
+			ParallelUtilsOpenMP::createTeam()->map(N, [&](size_t i) { cpuOutput[i] = cpuLambda2(i); });
 		}
 		{
 			auto timer = Timer::logScopeTiming("GPU");
-			int blockSize = 256;
-			int numBlocks = std::min(1 << 15, ceilDivision(N, blockSize));
-			opCuda << <numBlocks, blockSize >> > (N, inputGPU, yGPU, gpuLambda);
-			cudaDeviceSynchronize();
+			ParallelUtilsCuda::createTeam()->map(N, [=] __device__(int i) { gpuOutput[i] = gpuLambda(gpuFloats, gpuInput[i]); });
 		}
 		{
 			auto timer = Timer::logScopeTiming("GPU 2");
-			opCudaNested << <256, 1 >> > (N, inputGPU, yGPU, gpuLambda);
-			cudaDeviceSynchronize();
-		}
-		{
-			auto timer = Timer::logScopeTiming("GPU 3");
-			ParallelUtilsCuda::createTeam()->map(N, [=] __device__ (int i) { yGPU[i] = gpuLambda(inputGPU[i]); });
+			ParallelUtilsCuda::createTeam()->map(N, [=] __device__(int i) { gpuOutput[i] = gpuLambda2(i); });
 		}
 	}
 
 	{
 		auto timer = Timer::logScopeTiming("Transfer GPU to CPU");
-		cudaMemcpy(control.data(), yGPU, control.size() * sizeof(float), cudaMemcpyDeviceToHost);
+		cudaMemcpy(control.data(), gpuOutput, control.size() * sizeof(float), cudaMemcpyDeviceToHost);
 	}
 
 	float maxError = 0.0f;
 	for (int i = 0; i < N; i++)
 	{
-		float error = std::abs(control[i] - y[i]);
-		// if (error > 0.000001f)
-		//	std::cout << i << " " << y[i] << " " << control[i] << std::endl;
+		float error = std::abs(control[i] - cpuOutput[i]);
+		if (error > 0.000001f)
+		{
+			std::cout << i << " " << cpuOutput[i] << " " << control[i] << std::endl;
+			int a;
+			std::cin >> a;
+		}
 		maxError = std::max(maxError, error);
 	}
 	std::cout << "Max error CPU vs GPU: " << maxError << std::endl;
 
-	// Free memory
-	cudaFree(inputGPU);
-	cudaFree(yGPU);
+	cudaFree(gpuFloats);
+	cudaFree(gpuOutput);
+	cudaFree(gpuInput);
+	cudaFree(gpuNeighborIndices);
 
 	std::cin.get();
 
